a62c5f971886a2badf51c1ecbb330f1a60338c91
Ian Mucheru <imucheru.im@gmail.com>
2023-08-19 00:30:27 +0300

    Updated Active Section Scroll Link

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,6 +95,28 @@ const scrollUp = () => {
 window.addEventListener("scroll", scrollUp);
 
 /* =============== SCROLL SECTIONS ACTIVE LINK =============== */
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active__link");
+    } else {
+      sectionsClass.classList.remove("active__link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
 
 /* =============== DARK LIGHT THEME =============== */
 
